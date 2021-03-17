1dfcb07bfaa4d4be9ebf56d1db4fd729a07665f0
XamNalpak <maxkpaul21@gmail.com>
2021-03-17 12:05:47 -0400

    fixing imports

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="XamPy",
-    version="0.1.3.3",
+    version="0.1.4",
     author="Max Paul",
     author_email="maxkpaul21@gmail.com",
     description="A Data Science Package written in python.",
